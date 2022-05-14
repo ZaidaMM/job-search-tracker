6b9862ed4f8a29a062925258d80a166b8810a2f0
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-05-14 02:00:18 +0100

    Initial commit

---
+++ src/pages/Landing.js
@@ -0,0 +1,14 @@
+// import logo from '../assets/images/logo.svg';
+import main from '../assets/images/main.svg';
+
+const Landing = () => {
+  return (
+    <main>
+      <nav>
+        <h4>JST Job Search Tracker</h4>
+      </nav>
+    </main>
+  );
+};
+
+export default Landing;
