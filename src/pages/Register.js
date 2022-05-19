7cc4447605d11ed4c42d856f44c5b8e14ef06f67
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-05-19 05:03:09 +0100

    Register page setup

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,76 @@
-const Register = () => {
-  return <div>Register</div>;
+import { useEffect, useState } from 'react';
+import Wrapper from '../assets/wrappers/RegisterPage';
+import { Logo } from '../components';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
 };
+
+function Register() {
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+    console.log(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+
+        {/* Name input */}
+        <div className='form-row'>
+          <label htmlFor='name' className='form-label'>
+            Name
+          </label>
+          <input
+            type='text'
+            name='name'
+            value={values.name}
+            onChange={handleChange}
+            className='form-input'
+          />
+        </div>
+        {/* Email input */}
+        <div className='form-row'>
+          <label htmlFor='email' className='form-label'>
+            Email
+          </label>
+          <input
+            type='text'
+            name='email'
+            value={values.email}
+            onChange={handleChange}
+            className='form-input'
+          />
+        </div>
+        {/* Password input */}
+        <div className='form-row'>
+          <label htmlFor='password' className='form-label'>
+            Password
+          </label>
+          <input
+            type='password'
+            name='password'
+            value={values.password}
+            onChange={handleChange}
+            className='form-input'
+          />
+          <button type='submit' className='btn btn-block'>
+            Login
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
+}
 export default Register;
